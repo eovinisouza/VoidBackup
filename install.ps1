6cf3614e5cfb6574143b7dcb3df6aeebe486572d
eovinisouza <130377154+eovinisouza@users.noreply.github.com>
2026-02-04 20:43:43 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = "Stop"
 $repo = "eovinisouza/VoidBackup"
 $branch = "main"
 
-$steamPluginsPath = "${env:ProgramFiles(x86)}\Steam\plugins"
+$steamPluginsPath = "${env:ProgramFiles(x86)}\Steam\plugins\VoidBackup"
 
 Write-Host "Instalador VoidBackup iniciado..."
 
